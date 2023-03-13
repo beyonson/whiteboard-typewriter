77b147315fde52b9b291262c8233eacbb1fec71d
Justin Harnishfeger <jth80@pitt.edu>
2023-03-13 16:44:59 -0400

    Integration: Added draft of threading

--- main.py
+++ main.py
@@ -3,33 +3,45 @@ from queue import Queue
 import serial
 from pathfinding import *
 
+charPreProcThread = Thread(target=charPreProcProcess, args(1,))
+segmentationThread = Thread(target=segmentationProcess, args(2,))
+pathfindingThread = Thread(target=pathfindingProcess, args(3,))
+
 segmentationQueue = queue.Queue()
 pathfindingQueue = queue.Queue()
 serialToMotor = serial.Serial('port')
 
+charPreProcThread.start()
+segmentationThread.start()
+pathfindingThread.start()
+
 ###################################################
 ##      THREAD 1 : CHARACTER PREPROCESSING       ##
 
-# When letter recieved:
-#   Do skeletonization
-#   Add output to segmentation queue
+def charPreProcProcess():
+#   When letter recieved:
+#       Do skeletonization
+#       Add output to segmentation queue
 
 
 ###################################################
 ##      THREAD 2 : CHARACTER SEGMENTATION        ##
 
-while not segmentationQueue.empty():
-    # Take next letter from segmentation queue
-    # Perform character segmentation
-    # Add output list of lines to pathfinding queue
+def segmentationProcess():
+    while True:
+        while not segmentationQueue.empty():
+#           Take next letter from segmentation queue
+#           Perform character segmentation
+#           Add output list of lines to pathfinding queue
 
 ###################################################
 ##      THREAD 3 : PATHFINDING                   ##
 
-while True:
-    while not pathfindingQueue.empty():
-        lines = pathfindingQueue.get()
-        pathfinder = Pathfinder(lines)
-        pathfinder.pathfind()
-        pathfinder.convert()
-        serialToMotor(pathfinder.getGCode())
+def pathfindingProcess():
+    while True:
+        while not pathfindingQueue.empty():
+            lines = pathfindingQueue.get()
+            pathfinder = Pathfinder(lines)
+            pathfinder.pathfind()
+            pathfinder.convert()
+            serialToMotor(pathfinder.getGCode())
