373446a0dcfc29734bd891ff50ca55941326f4ec
beyonson <garrettmarcinak@gmail.com>
2023-03-29 17:16:30 -0400

    update submodule to track main

--- ui/Thinning.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
- 
-# Create an image with text on it
-img1 = cv2.imread('./binarized/ss0.jpg') 
-thin = cv2.ximgproc.thinning(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY))
-cv2.imshow('thinned',thin)
-cv2.waitKey(0)
