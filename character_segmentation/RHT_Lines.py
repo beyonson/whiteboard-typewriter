0bdb0db0bff17275e78f3b00a2b515e1855cf87e
Yasser Morsy <54084895+YKMorsy@users.noreply.github.com>
2023-04-14 09:56:14 -0400

    Create RHT_Lines.py

---
+++ character_segmentation/RHT_Lines.py
@@ -0,0 +1,42 @@
+import random
+
+def rht(image, num_iterations, tolerance, threshold):
+    active_points = find_active_points(image)
+    accumulator = {}
+    for i in range(num_iterations):
+        p1, p2 = random.sample(active_points, 2)
+        a, b = solve_line_equation(p1, p2)
+        for key in accumulator:
+            if within_tolerance(key, (a,b), tolerance):
+                accumulator[key] += 1
+                break
+        else:
+            accumulator[(a,b)] = 1
+        if accumulator[(a,b)] >= threshold:
+            mark_line_in_image(image, (a,b))
+    return image
+
+def find_active_points(image):
+    active_points = []
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if image[i,j] == 1:
+                active_points.append((i,j))
+    return active_points
+
+def solve_line_equation(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    a = (y2 - y1) / (x2 - x1)
+    b = y1 - a*x1
+    return a, b
+
+def within_tolerance(p1, p2, tolerance):
+    return abs(p1[0] - p2[0]) <= tolerance and abs(p1[1] - p2[1]) <= tolerance
+
+def mark_line_in_image(image, line):
+    a, b = line
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if abs(a*i + b - j) <= 1:
+                image[i,j] = 2
